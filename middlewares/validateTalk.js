fbcb8dfc16b96216b8fcfb256e0f6574ceb2009e
pand-oly <hoa98hoa@gmail.com>
2022-08-03 18:43:19 -0300

    Create validate talk function in new file in middlewares floder

---
+++ middlewares/validateTalk.js
@@ -0,0 +1,9 @@
+function validateTalk(req, res, next) {
+  const { talk } = req.body;
+
+  if (!talk) return res.status(400).json({ message: 'O campo "talk" é obrigatório' });
+
+  next();
+}
+
+module.exports = validateTalk;
