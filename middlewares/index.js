084ffa3f186664c2353b85da0232393bc1f0abc1
pand-oly <hoa98hoa@gmail.com>
2022-08-03 18:46:33 -0300

    Pass the new functions in middleware/index.js

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,7 +1,21 @@
 const validateEmail = require('./validateEmail');
 const validatePassword = require('./validatePassword');
+const checkAuthorization = require('./checkAuthorization');
+const validateToken = require('./validateToken');
+const validateName = require('./validateName');
+const validateAge = require('./validateAge');
+const validateTalk = require('./validateTalk');
+const validateWatchedAt = require('./validateWatchedAt');
+const validateRate = require('./validateRate');
 
 module.exports = {
   validateEmail,
   validatePassword,
+  checkAuthorization,
+  validateToken,
+  validateName,
+  validateAge,
+  validateTalk,
+  validateWatchedAt,
+  validateRate,
 };
