9762739da980536220604a2550a37dde34304434
pand-oly <hoa98hoa@gmail.com>
2022-08-03 18:51:18 -0300

    Pass generate token in middleware/index.js

--- middlewares/index.js
+++ middlewares/index.js
@@ -7,6 +7,7 @@ const validateAge = require('./validateAge');
 const validateTalk = require('./validateTalk');
 const validateWatchedAt = require('./validateWatchedAt');
 const validateRate = require('./validateRate');
+const generateToken = require('./generateToken');
 
 module.exports = {
   validateEmail,
@@ -18,4 +19,5 @@ module.exports = {
   validateTalk,
   validateWatchedAt,
   validateRate,
+  generateToken,
 };
